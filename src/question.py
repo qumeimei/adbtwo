3ceef24c63cf623b7a7994c6530f9ef3e074423a
MackeyZheng <bearzheng2011@gmail.com>
2014-04-03 13:13:16 -0400

    update question model

--- src/question.py
+++ src/question.py
@@ -2,5 +2,9 @@ class Question:
 	def __init__(self, api_key):
 		self.api_key = api_key
 	
-	def run(self, query):
-		print query
+	def run(self, query, single = True):
+		query = query.strip()
+		if single:
+			print 'Let me see...'
+		else:
+			print 'Query-Question: ' + query
