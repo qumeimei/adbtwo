6016c69ab3a76146ba70b0698c7a13ffdf904c0b
Jingyi Guo <ejingyiguo@gmail.com>
2014-04-04 00:01:43 -0400

    modified readme and question.py

--- src/question.py
+++ src/question.py
@@ -1,6 +1,5 @@
 import json
 import urllib
-from pprint import pprint
 from util import *
 
 
@@ -31,59 +30,66 @@ class Question:
 		print '\n\n'
 		#api_key = open('.api_key').read()
 		question=query.strip()
-
 		query1=question.strip()
 		query1 = ' '.join(question.split()[2:])
 		suffix="?"
+		que=Question(self.api_key)
 		if query1.endswith(suffix) == True:
 			query1 = query1[:-1]
 			bus_dict=dict()
-			que=Question(self.api_key)
 			dict1=que.mqlread(bus_dict,query1,CONFINE[0],CONFINE[1])
 			dict2=que.mqlread(dict1,query1,CONFINE[2],CONFINE[3])
-	
-		x=1
-		sl=101
-		str=que.pl(sl,1)
-		str +='	|'+' '*((sl-len(question))/2)+question+' '*((sl-len(question))/2)+' |'+'\n'
-		str +=que.pl(sl,1)
-	
-		for key in sorted(dict2.iterkeys()):
 		
+		
+		sl=101
+		sll=sl+4
+		str=que.pl(sll,1)
+		str +='	|'+' '*((sll-len(question))/2)+question+' '*((sll-len(question))/2)+' |'+'\n'
+		
+		
+		word_max=0
+		names=[]
+		for key in sorted(dict2.iterkeys()):
+			key1 = ' '.join(key.split()[0:len(key.split())-3])
+			word_max=max(word_max, len(key1))
+			names.append(key1)
+		#print "word_max is: %d " %(word_max)
+		
+		x=0
+		for key in sorted(dict2.iterkeys()):
+			
 			key1 = ' '.join(key.split()[0:len(key.split())-3])
 			typ = ' '.join(key.split()[len(key.split())-2:len(key.split())-1])
 			typ = typ[:-1]
-			str+=que.prt(key1,'As','Creation')
-			y=1
-			str+='	|'+que.stl(20,81)
-			for num in dict2[key]:
-				if y==1:
-					str+=que.prt(' ',typ,num)
-				else:
-					str+=que.prt(' ',' ',num)
-				if y==len(dict2[key]):
-					str +=que.pl(sl,1)
-				y+=1
+			
+			
+			if key1==names[x-1]:
+				y=1
+				for num in dict2[key]:
+					if y==1:
+						str+=que.prt(' ',typ,num,word_max,(sl-word_max)/2)
+					else:
+						str+=que.prt(' ',' ',num,word_max,(sl-word_max)/2)	
+					y+=1
+					
+			else:
+				str +=que.pl(sll,1)
+				str+=que.prt(key1,'As','Creation',word_max,(sl-word_max)/2)
+				str+='	|'+que.stl(word_max+2,sl-word_max+2)
+				y=1
+				for num in dict2[key]:
+					if y==1:
+						str+=que.prt(' ',typ,num,word_max,(sl-word_max)/2)
+					else:
+						str+=que.prt(' ',' ',num,word_max,(sl-word_max)/2)	
+					y+=1
+				'''if y==len(dict2[key]):
+					str +=que.pl(sll,1)
+					'''
+			x+=1
+		str +=que.pl(sll,1)	
 		print str
 	
-		x=1
-		'''for key in sorted(dict2.iterkeys()):
-			str=key
-			z=1
-			for num in dict2[key]:
-				if z==len(dict2[key]) and z>1:
-					str+= ', and '+'<'+num+'> '
-				else:
-					if z>1:
-						str+= ', <'+num+'>'
-					else:
-						str+= '<'+num+'>'
-				z+=1
-			print "%d"  %(x)+'. ' +str
-			x+=1
-			'''
-
-
 	def mqlread(self, bus_dict,key_word,confine_word,confine):
 	
 		service_url = 'https://www.googleapis.com/freebase/v1/mqlread'
@@ -100,7 +106,6 @@ class Question:
 		else:
 			modify=IDENTITY[1]
 		for planet in response['result']:
-	#print planet['name']
 			for anb in planet[confine_word]:
 				if planet['name']+modify in bus_dict.keys():
 					bus_dict[planet['name']+modify].append(anb['a:name'])
@@ -111,5 +116,5 @@ class Question:
 		return '	 '*num_tab+formatLine("-"*lenthofline, lenthofline, single = True)+'\n'
 	def stl(self,before,after):
 		return ' '*before+formatLine("-"*after, after, single = True)+'\n'
-	def prt(self,nu1,nu2,nu3):
-		return  '	|'+formatLine(' '+nu1, 20, single = True)+'|'+formatLine(nu2, 39, single = True)+'| '+formatLine(nu3, 39, single = True)+'|'+'\n'
+	def prt(self,nu1,nu2,nu3,num4,num5):
+		return  '	|'+formatLine(' '+nu1, num4+2, single = True)+'|'+formatLine(nu2, num5, single = True)+'| '+formatLine(nu3, num5, single = True)+'|'+'\n'
